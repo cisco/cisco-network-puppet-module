cisco/cisco-network-puppet-module

commit be4c34fcba1e34f92eae674d5904d146e3091b96

Improvements to provider selection and gem dependency checking

lib/puppet/feature/cisco_node_utils.rb
@@ -1,4 +1,23 @@
 require 'puppet/util/feature'
 
-# We have the cisco_node_utils gem
-Puppet.features.add(:cisco_node_utils, :libs => ['cisco_node_utils'])
+# We have the cisco_node_utils gem at least version 1.0.0
+Puppet.features.add :cisco_node_utils do
+  begin
+    require 'cisco_node_utils'
+    min_version = Gem::Version.new('0.9.0')
+    rec_version = Gem::Version.new('1.0.0')
+    gem_version = Gem::Version.new(CiscoNodeUtils::VERSION)
+    if gem_version < min_version
+      error "This module requires version #{min_version} or later of the "\
+            "'cisco_node_utils' gem but version #{gem_version} is installed. "\
+            'Please upgrade.'
+    elsif gem_version < rec_version
+      warn "This module works best with version #{rec_version} of the "\
+           "'cisco_node_utils' gem but version #{gem_version} is installed. "\
+           'Some features may not be fully functional.'
+    end
+    gem_version >= min_version
+  rescue LoadError
+    false
+  end
+end

lib/facter/cisco_node_utils.rb
@@ -1,0 +1,17 @@
+require 'facter'
+
+Facter.add(:cisco_node_utils) do
+  # Facter isn't aware of Puppet features so we have to check for the gem:
+  confine do
+    begin
+      require 'cisco_node_utils'
+      true
+    rescue LoadError
+      false
+    end
+  end
+
+  setcode do
+    CiscoNodeUtils::VERSION
+  end
+end

lib/facter/cisco.rb
@@ -1,16 +1,11 @@
 require 'facter'
 
 Facter.add(:cisco) do
-  # Facter isn't aware of Puppet features so we have to check for the gem:
-  confine do
-    begin
-      require 'cisco_node_utils'
-      true
-    rescue LoadError
-      false
-    end
+  confine :operatingsystem => :nexus
+  confine :cisco_node_utils do
+    # Any version is OK so long as it is installed
+    true
   end
-  confine :kernel => 'Linux' # TODO be more specific
 
   setcode do
     hash = {}

lib/facter/cisco_nxapi.rb
@@ -1,0 +1,16 @@
+require 'facter'
+
+Facter.add(:cisco_nxapi) do
+  confine do
+    begin
+      require 'cisco_nxapi'
+      true
+    rescue LoadError
+      false
+    end
+  end
+
+  setcode do
+    CiscoNxapi::VERSION
+  end
+end

lib/puppet/provider/cisco_command_config/nxapi.rb
@@ -21,6 +21,7 @@ require 'cisco_node_utils' if Puppet.features.cisco_node_utils?
 Puppet::Type.type(:cisco_command_config).provide(:nxapi) do
 
   confine :feature => :cisco_node_utils
+  defaultfor :operatingsystem => :nexus
 
   def initialize(value={})
     super(value)

lib/puppet/provider/cisco_interface/nxapi.rb
@@ -30,6 +30,7 @@ Puppet::Type.type(:cisco_interface).provide(:nxapi) do
   desc "The NXAPI provider for cisco_interface."
 
   confine :feature => :cisco_node_utils
+  defaultfor :operatingsystem => :nexus
 
   mk_resource_methods
 

lib/puppet/provider/cisco_interface_ospf/nxapi.rb
@@ -29,6 +29,7 @@ Puppet::Type.type(:cisco_interface_ospf).provide(:nxapi) do
   desc "The nxapi provider."
 
   confine :feature => :cisco_node_utils
+  defaultfor :operatingsystem => :nexus
 
   mk_resource_methods
 

lib/puppet/provider/cisco_ospf/nxapi.rb
@@ -22,6 +22,7 @@ Puppet::Type.type(:cisco_ospf).provide(:nxapi) do
   desc "The NXAPI provider."
 
   confine :feature => :cisco_node_utils
+  defaultfor :operatingsystem => :nexus
 
   mk_resource_methods
 

commit 6129fd6013a6ba2e4ceca96a434ca6671cf471d8

Recover 'lazy-loading' behavior broken by be4c34fc

lib/puppet/feature/cisco_node_utils.rb
@@ -1,23 +1,51 @@
 require 'puppet/util/feature'
 
-# We have the cisco_node_utils gem at least version 1.0.0
-Puppet.features.add :cisco_node_utils do
-  begin
-    require 'cisco_node_utils'
-    min_version = Gem::Version.new('0.9.0')
-    rec_version = Gem::Version.new('1.0.0')
-    gem_version = Gem::Version.new(CiscoNodeUtils::VERSION)
-    if gem_version < min_version
-      error "This module requires version #{min_version} or later of the "\
-            "'cisco_node_utils' gem but version #{gem_version} is installed. "\
-            'Please upgrade.'
-    elsif gem_version < rec_version
-      warn "This module works best with version #{rec_version} of the "\
-           "'cisco_node_utils' gem but version #{gem_version} is installed. "\
-           'Some features may not be fully functional.'
+# We have the cisco_node_utils gem
+Puppet.features.add(:cisco_node_utils, libs: ['cisco_node_utils'])
+
+# TODO: simplify if https://tickets.puppetlabs.com/browse/PUP-1159 gets fixed.
+# Override the default caching logic created in Puppet.features:
+#
+# meta_def('cisco_node_utils?') do
+#   # we return a cached result if:
+#   #  * if a block is given (and we just evaluated it above)
+#   #  * if we already have a positive result
+#   #  * if we've tested this feature before and it failed, but we're
+#   #    configured to always cache
+#   if block_given? ||
+#      @results['cisco_node_utils'] ||
+#      (@results.has_key?('cisco_node_utils') and
+#       Puppet[:always_cache_features])
+#     @results['cisco_node_utils']
+#   else
+#     @results['cisco_node_utils'] = test('cisco_node_utils',
+#                                         libs: ['cisco_node_utils'])
+#     @results['cisco_node_utils']
+#   end
+# end
+
+class <<Puppet.features
+  def cisco_node_utils?
+    # we return a cached result if:
+    #  * if we already have a positive result
+    #  * if we've tested this feature before and it failed, but we're
+    #    configured to always cache
+    if @results['cisco_node_utils'] ||
+       (@results.key?('cisco_node_utils') && Puppet[:always_cache_features])
+      @results['cisco_node_utils']
+    else
+      @results['cisco_node_utils'] = test('cisco_node_utils',
+                                          libs: ['cisco_node_utils'])
+      if @results['cisco_node_utils']
+        rec_version = Gem::Version.new('1.0.0')
+        gem_version = Gem::Version.new(CiscoNodeUtils::VERSION)
+        if gem_version < rec_version
+          warn "This module works best with version #{rec_version} of gem "\
+               "'cisco_node_utils' but version #{gem_version} is installed. "\
+               'Some features may not be fully functional.'
+        end
+      end
+      @results['cisco_node_utils']
     end
-    gem_version >= min_version
-  rescue LoadError
-    false
   end
 end