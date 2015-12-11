50cea24dca543ce3fe3c8919e9032b2a8975c82a
sai chintalapudi <saichint@cisco.com>
2015-12-11 11:50:51 -0800

    beaker tests for tacacs fixed

--- tests/beaker_tests/tacacsserverhost/tacacsserverhost_provider_negatives.rb
+++ tests/beaker_tests/tacacsserverhost/tacacsserverhost_provider_negatives.rb
@@ -76,15 +76,6 @@ test_name "TestCase :: #{testheader}" do
       'agent -t', options)
     on(agent, cmd_str, acceptable_exit_codes: [0, 2])
 
-    # Expected exit_code is 16 since this is a vegas shell cmd with exec error.
-    # Flag is set to true to check for absence of RegExp pattern in stdout.
-    cmd_str = UtilityLib.get_vshell_cmd('show running-config tacacs')
-    on(agent, cmd_str, acceptable_exit_codes: [16]) do
-      UtilityLib.search_pattern_in_output(stdout,
-                                          [/feature tacacs\+/],
-                                          true, self, logger)
-    end
-
     logger.info("Setup switch for provider test :: #{result}")
   end
 
