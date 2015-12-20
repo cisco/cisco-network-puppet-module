25b9010c62dce6b3f83f1bea93d05b51219e7a20
Alex Hunsberger <aahunsberger@gmail.com>
2015-12-20 17:00:23 -0500

    aaa login exec svc

--- examples/demo_aaa.pp
+++ examples/demo_aaa.pp
@@ -15,22 +15,41 @@
 # limitations under the License.
 
 class ciscopuppet::demo_aaa {
-    cisco_tacacs_server_host { 'testhost':
-      ensure                 => present,
+  cisco_tacacs_server { 'default':
+    ensure              => present,
+    encryption_type     => clear,
+    encryption_password => 'testing123',
+    source_interface    => 'mgmt0',
   }
-    cisco_aaa_group_tacacs { 'test':
-      ensure                 => present,
-      deadtime               => '30',
-      server_hosts           => ['testhost'],
-      source_interface       => 'Ethernet1/1',
-      vrf_name               => 'blue',
-      require                => Cisco_tacacs_server_host['testhost']
+  cisco_tacacs_server_host { '1.1.1.1':
+    ensure              => present,
+    encryption_type     => 'encrypted',
+    encryption_password => 'testing123',
+    require             => Cisco_tacacs_server['default'],
   }
-    cisco_aaa_authentication_login { 'default':
-      ascii_authentication => 'true',
-      chap                 => 'false',
-      error_display        => 'true',
-      mschap               => 'false',
-      mschapv2             => 'false',
+  cisco_aaa_group_tacacs { 'group1':
+    ensure           => present,
+    deadtime         => '30',
+    server_hosts     => ['1.1.1.1'],
+    source_interface => 'mgmt0',
+    vrf_name         => 'management',
+    require          => Cisco_tacacs_server_host['1.1.1.1']
+  }
+  cisco_aaa_authentication_login { 'default':
+    ascii_authentication => 'true',
+    chap                 => 'false',
+    error_display        => 'true',
+    mschap               => 'false',
+    mschapv2             => 'false',
+  }
+  cisco_aaa_authorization_login_cfg_svc { 'default':
+    ensure => 'present',
+    groups => ["group1"],
+    method => 'local',
+  }
+  cisco_aaa_authorization_login_exec_svc { 'default':
+    ensure => 'present',
+    groups => ["group1"],
+    method => 'local',
   }
 }
