8a15f6e36e1acea8962d04574a835701ac5c492a
sai chintalapudi <saichint@cisco.com>
2016-01-19 16:06:50 -0800

    stp per intf

--- examples/demo_interface.pp
+++ examples/demo_interface.pp
@@ -15,23 +15,59 @@
 # limitations under the License.
 
 class ciscopuppet::demo_interface {
+
+  $stp_bpdufilter = platform_get() ? {
+    'n7k'  => 'enable',
+    default => undef
+  }
+
+  $stp_bpduguard = platform_get() ? {
+    'n7k'  => 'enable',
+    default => undef
+  }
+
+  $stp_cost = platform_get() ? {
+    'n7k'  => 2000,
+    default => undef
+  }
+
+  $stp_guard = platform_get() ? {
+    'n7k'  => 'loop',
+    default => undef
+  }
+
+  $stp_link_type = platform_get() ? {
+    'n7k'  => 'shared',
+    default => undef
+  }
+
+  $stp_port_priority = platform_get() ? {
+    'n7k'  => 64,
+    default => undef
+  }
+
+  $stp_port_type = platform_get() ? {
+    'n7k'  => 'network',
+    default => undef
+  }
+
   cisco_interface { 'Ethernet1/1' :
-    shutdown                       => true,
-    switchport_mode                => disabled,
-    description                    => 'managed by puppet',
-    ipv4_address                   => '192.168.55.5',
-    ipv4_netmask_length            => 24,
-    ipv4_address_secondary         => '192.168.88.1',
-    ipv4_netmask_length_secondary  => 24,
-    ipv4_pim_sparse_mode           => false,
-    mtu                            => 1600,
-    speed                          => 100,
-    duplex                         => 'full',
-    vrf                            => 'test',
-    ipv4_acl_in                    => 'v4acl1',
-    ipv4_acl_out                   => 'v4acl2',
-    ipv6_acl_in                    => 'v6acl1',
-    ipv6_acl_out                   => 'v6acl2',
+    shutdown                      => true,
+    switchport_mode               => disabled,
+    description                   => 'managed by puppet',
+    ipv4_address                  => '192.168.55.5',
+    ipv4_netmask_length           => 24,
+    ipv4_address_secondary        => '192.168.88.1',
+    ipv4_netmask_length_secondary => 24,
+    ipv4_pim_sparse_mode          => false,
+    mtu                           => 1600,
+    speed                         => 100,
+    duplex                        => 'full',
+    vrf                           => 'test',
+    ipv4_acl_in                   => 'v4acl1',
+    ipv4_acl_out                  => 'v4acl2',
+    ipv6_acl_in                   => 'v6acl1',
+    ipv6_acl_out                  => 'v6acl2',
   }
 
   cisco_interface { 'Ethernet1/1.1':
@@ -50,6 +86,16 @@ class ciscopuppet::demo_interface {
     switchport_mode               => trunk,
     switchport_trunk_allowed_vlan => '20, 30',
     switchport_trunk_native_vlan  => 40,
+  }
+
+  cisco_interface { 'Ethernet1/4':
+    stp_bpdufilter    => $stp_bpdufilter,
+    stp_bpduguard     => $stp_bpduguard,
+    stp_cost          => $stp_cost,
+    stp_guard         => $stp_guard,
+    stp_link_type     => $stp_link_type,
+    stp_port_priority => $stp_port_priority,
+    stp_port_type     => $stp_port_type,
   }
 
   cisco_interface { 'Vlan22':
