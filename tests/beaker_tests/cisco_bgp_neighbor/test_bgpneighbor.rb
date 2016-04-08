53f2ccd1217635bcbd5c5a55194effa422a1eb7c
Chris Van Heuveln <cvanheuv@cisco.com>
2016-04-08 10:59:35 -0400

    test_bgp beaker cleanups
    * Consolidated multiple non_default tests into just a few
    * Updated for platform differences and unprops syntax

--- tests/beaker_tests/cisco_bgp_neighbor/test_bgpneighbor.rb
+++ tests/beaker_tests/cisco_bgp_neighbor/test_bgpneighbor.rb
@@ -59,8 +59,8 @@ tests[:default] = {
   },
 }
 
-tests[:non_def_uber] = {
-  desc:           'Non Default: (UBER)',
+tests[:non_default] = {
+  desc:           '2.1 Non Default',
   preclean:       'cisco_bgp_neighbor',
   title_pattern:  '2 default 1.1.1.1',
   manifest_props: {
@@ -77,26 +77,17 @@ tests[:non_def_uber] = {
     suppress_4_byte_as:     'true',
     timers_keepalive:       '90',
     timers_holdtime:        '270',
+    update_source:          'loopback151',
   },
 }
 
 tests[:non_def_local_remote_as] = {
   preclean:       'cisco_bgp_neighbor',
   title_pattern:  '2 default 1.1.1.1',
-  desc:           'Non Default: (AS) local-as, remote-as',
+  desc:           '2.2 Non Default: (AS) local-as, remote-as',
   manifest_props: {
     local_as:  '42',
     remote_as: '12.1',
-  },
-}
-
-tests[:non_def_update_source] = {
-  desc:           'Non Default: (update-source)',
-  title_pattern:  '2 default 1.1.1.1',
-  intf_type:      'ethernet',
-  manifest_props: {
-    remote_as: '12.1', # must be set for IOS_XR
-    # update_source: find_interface(tests, id) # Set dynamically at run time
   },
 }
 
@@ -163,16 +154,11 @@ test_name "TestCase :: #{tests[:resource_name]}" do
 
   # -------------------------------------------------------------------
   logger.info("\n#{'-' * 60}\nSection 2. Non Default Property Testing")
-  test_harness_run(tests, :non_def_uber)
-  test_harness_bgp_vrf(tests, :non_def_uber, 'blue')
+  test_harness_run(tests, :non_default)
+  test_harness_bgp_vrf(tests, :non_default, 'blue')
 
   test_harness_run(tests, :non_def_local_remote_as)
   test_harness_bgp_vrf(tests, :non_def_local_remote_as, 'blue')
-
-  id = :non_def_update_source
-  tests[id][:manifest_props][:update_source] = find_interface(tests, id)
-  test_harness_run(tests, id)
-  test_harness_bgp_vrf(tests, id, 'blue')
 
   # -------------------------------------------------------------------
   logger.info("\n#{'-' * 60}\nSection 3. Title Pattern Testing")
