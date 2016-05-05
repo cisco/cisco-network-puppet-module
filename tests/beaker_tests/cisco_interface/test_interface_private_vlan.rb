36778b277a41ef6685d2124e047e00a88219efa1
Chris Van Heuveln <cvanheuv@cisco.com>
2016-05-05 15:13:25 -0400

    Refactor test_interface.rb into multiple files
    * This test was trying to do too much; it became too large and unwieldy to use and troubleshoot.
    * Refactor to use new test syntax (test_harness_run)
    * File changes:
      *  deleted:    test_interface.rb
      *  new file:   interfacelib.rb
      *  new file:   test_interface_L2.rb
      *  new file:   test_interface_L3.rb
      *  new file:   test_interface_bdi.rb
      *  modified:   test_interface_capabilities.rb
      *  modified:   test_interface_stp.rb
      *  new file:   test_interface_svi.rb
      *  new file:   test_interface_private_vlan.rb
    
    * renamed test_vlan_mapping.rb to test_interface_vlan_mapping.rb
    
    * Only tested on n7k and n9k so far but there's a lot here and I wanted to get some eyes on the changes

---
+++ tests/beaker_tests/cisco_interface/test_interface_private_vlan.rb
@@ -0,0 +1,188 @@
+###############################################################################
+# Copyright (c) 2016 Cisco and/or its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+#
+# See README-develop-beaker-scripts.md (Section: Test Script Variable Reference)
+# for information regarding:
+#  - test script general prequisites
+#  - command return codes
+#  - A description of the 'tests' hash and its usage
+#
+###############################################################################
+#
+# 'test_interface_private_vlan' tests private_vlan interface properties.
+#
+###############################################################################
+require File.expand_path('../interfacelib.rb', __FILE__)
+
+# Test hash top-level keys
+tests = {
+  agent:            agent,
+  master:           master,
+  intf_type:        'ethernet',
+  operating_system: 'nexus',
+  resource_name:    'cisco_interface',
+}
+
+# Assign a test interface.
+intf = find_interface(tests)
+
+# Test hash test cases
+tests[:default] = {
+  desc:               '1.1 Default Properties',
+  title_pattern:      intf,
+  code:               [0],
+  preclean_intf:      true,
+  sys_def_switchport: true,
+  manifest_props:     {
+    # switchport_mode_private_vlan_host:             no default
+    switchport_mode_private_vlan_host_association:  'default',
+    switchport_mode_private_vlan_trunk_promiscuous: 'default',
+    switchport_mode_private_vlan_trunk_secondary:   'default',
+    # TBD: BROKEN. Should support 'default' but sends literal 'default'
+    # instead of doing default behavior.
+    # switchport_private_vlan_trunk_allowed_vlan:     'default',
+    switchport_private_vlan_association_trunk:      'default',
+    switchport_private_vlan_mapping_trunk:          'default',
+    switchport_private_vlan_trunk_native_vlan:      'default',
+  },
+  resource:           {
+    switchport_mode_private_vlan_host:              'disabled',
+    # switchport_mode_private_vlan_host_association: nil,
+    switchport_mode_private_vlan_trunk_promiscuous: 'false',
+    switchport_mode_private_vlan_trunk_secondary:   'false',
+    # switchport_private_vlan_trunk_allowed_vlan:    nil,
+    # switchport_private_vlan_association_trunk:     nil,
+    # switchport_private_vlan_mapping_trunk:         nil,
+    switchport_private_vlan_trunk_native_vlan:      '1',
+  },
+}
+
+# allowed = ['100', '102-103', '105']
+assoc = %w(106 107)
+tests[:port_mode_host] = {
+  desc:               '2.1 Port Mode Host',
+  title_pattern:      intf,
+  preclean_intf:      true,
+  sys_def_switchport: true,
+  manifest_props:     {
+    switchport_mode_private_vlan_host:             :host,
+    switchport_mode_private_vlan_host_association: assoc,
+    switchport_private_vlan_trunk_native_vlan:     100,
+    # switchport_private_vlan_trunk_allowed_vlan:    allowed,
+  },
+  resource:           {
+    switchport_mode_private_vlan_host:             'host',
+    switchport_mode_private_vlan_host_association: "#{assoc}",
+    switchport_private_vlan_trunk_native_vlan:     '100',
+    # TBD: BROKEN: Idempotence issues. This implementation is broken:
+    #   ['100', '102-103', '105'] vs '100 102-103 105'
+    # Why does it transform from multi-member list to string???
+    # switchport_private_vlan_trunk_allowed_vlan:    "#{allowed}",
+  },
+}
+
+# Clone the test hash above to retest using 'promiscuous' mode
+t = tests[:port_mode_promiscuous] = tests[:port_mode_host].clone
+t[:desc] = '2.2 Port Mode Promiscuous'
+t[:manifest_props][:switchport_mode_private_vlan_host] = :promiscuous
+t[:resource][:switchport_mode_private_vlan_host] = :promiscuous
+
+tests[:trunk_secondary] = {
+  desc:               '3.1 Trunk Secondary',
+  platform:           'n(5|6|7|9)k',
+  title_pattern:      intf,
+  preclean_intf:      true,
+  sys_def_switchport: true,
+  manifest_props:     {
+    switchport_mode_private_vlan_trunk_secondary: true,
+    switchport_private_vlan_association_trunk:    %w(100 102),
+    switchport_private_vlan_mapping_trunk:        ['100', '101,104-105'],
+  },
+  resource:           {
+    switchport_mode_private_vlan_trunk_secondary: 'true',
+    # TBD: BROKEN. It inputs a multi-member list but normalizes to a
+    # single index? ['100 102'] is wrong but we will keep this temporarily to
+    # show the broken behavior.
+    switchport_private_vlan_association_trunk:    "['100 102']",
+    # TBD: BROKEN. Same as above.
+    switchport_private_vlan_mapping_trunk:        "['100 101,104-105']",
+  },
+}
+tests[:trunk_promiscuous] = {
+  desc:               '3.2 Trunk Promiscuous',
+  platform:           'n(5|6|7|9)k',
+  title_pattern:      intf,
+  preclean_intf:      true,
+  sys_def_switchport: true,
+  manifest_props:     {
+    switchport_mode_private_vlan_trunk_promiscuous: true,
+    switchport_private_vlan_association_trunk:      %w(100 102),
+    switchport_private_vlan_mapping_trunk:          ['100', '101,104-105'],
+  },
+  resource:           {
+    switchport_mode_private_vlan_trunk_promiscuous: 'true',
+    # TBD: BROKEN. It inputs a multi-member list but normalizes to a
+    # single index? ['100 102'] is wrong but we will keep this temporarily to
+    # show the broken behavior.
+    switchport_private_vlan_association_trunk:      "['100 102']",
+    # TBD: BROKEN. Same as above.
+    switchport_private_vlan_mapping_trunk:          "['100 101,104-105']",
+  },
+}
+
+svi = 'vlan13'
+mapping = %w(108-109)
+tests[:svi_mapping] = {
+  desc:           '4.1 SVI Private-Vlan Mapping',
+  platform:       'n(5|6|7|9)k',
+  title_pattern:  svi,
+  preclean_intf:  true,
+  manifest_props: {
+    private_vlan_mapping: mapping
+  },
+  resource:       {
+    private_vlan_mapping: "#{mapping}"
+  },
+}
+
+#################################################################
+# TEST CASE EXECUTION
+#################################################################
+test_name "TestCase :: #{tests[:resource_name]}" do
+  # -------------------------------------------------------------------
+  logger.info("\n#{'-' * 60}\nSection 1. Defaults")
+  test_harness_run(tests, :default)
+
+  # -------------------------------------------------------------------
+  logger.info("\n#{'-' * 60}\nSection 2. Port Mode")
+  test_harness_run(tests, :port_mode_host)
+  test_harness_run(tests, :port_mode_promiscuous)
+
+  # -------------------------------------------------------------------
+  logger.info("\n#{'-' * 60}\nSection 3. Trunk Mode")
+  test_harness_run(tests, :trunk_secondary)
+  test_harness_run(tests, :trunk_promiscuous)
+
+  # -------------------------------------------------------------------
+  logger.info("\n#{'-' * 60}\nSection 4. SVI Mapping")
+  test_harness_run(tests, :svi_mapping)
+
+  # -------------------------------------------------------------------
+  interface_cleanup(agent, intf)
+  remove_interface(agent, svi)
+end
+
+logger.info("TestCase :: #{tests[:resource_name]} :: End")
--- tests/beaker_tests/cisco_interface/test_interface_stp.rb
+++ tests/beaker_tests/cisco_interface/test_interface_stp.rb
@@ -21,21 +21,30 @@
 #  - A description of the 'tests' hash and its usage
 #
 ###############################################################################
-require File.expand_path('../../lib/utilitylib.rb', __FILE__)
+#
+# 'test_interface_stp' primarily tests STP interface properties.
+#
+###############################################################################
+require File.expand_path('../interfacelib.rb', __FILE__)
 
 # Test hash top-level keys
 tests = {
-  master:           master,
   agent:            agent,
+  master:           master,
+  intf_type:        'ethernet',
   operating_system: 'nexus',
   resource_name:    'cisco_interface',
 }
 
+# Find a usable interface for this test
+intf = find_interface(tests)
+
 # Test hash test cases
 tests[:default] = {
   desc:           '1.1 Default Properties',
-  intf_type:      'ethernet',
-  preclean:       'cisco_interface',
+  title_pattern:  intf,
+  preclean_intf:  true,
+  code:           [0],
   manifest_props: {
     stp_bpdufilter:         'default',
     stp_bpduguard:          'default',
@@ -64,15 +73,13 @@ tests[:default] = {
   },
 }
 
-# Non-default Tests. NOTE: [:resource] = [:manifest_props] for all non-default
-
 stp_mst_cost_ndp = Array[%w(0,2-4,6,8-12 1000), %w(1000 2568)]
 stp_mst_port_priority_ndp = Array[%w(0,2-11,20-33 64), %w(1111 160)]
 stp_vlan_cost_ndp = Array[%w(1-4,6,8-12 1000), %w(1000 2568)]
 stp_vlan_port_priority_ndp = Array[%w(1-11,20-33 64), %w(1111 160)]
 tests[:non_default] = {
   desc:           '2.1 Non Defaults',
-  intf_type:      'ethernet',
+  title_pattern:  intf,
   manifest_props: {
     switchport_mode:        'trunk',
     stp_bpdufilter:         'enable',
@@ -88,6 +95,14 @@ tests[:non_default] = {
     stp_vlan_port_priority: stp_vlan_port_priority_ndp,
   },
   resource:       {
+    switchport_mode:        'trunk',
+    stp_bpdufilter:         'enable',
+    stp_bpduguard:          'enable',
+    stp_cost:               '2000',
+    stp_guard:              'loop',
+    stp_link_type:          'shared',
+    stp_port_priority:      '64',
+    stp_port_type:          'network',
     stp_mst_cost:           "#{stp_mst_cost_ndp}",
     stp_mst_port_priority:  "#{stp_mst_port_priority_ndp}",
     stp_vlan_cost:          "#{stp_vlan_cost_ndp}",
@@ -95,65 +110,21 @@ tests[:non_default] = {
   },
 }
 
-# Create actual manifest for a given test scenario.
-def build_manifest_interface(tests, id)
-  manifest = prop_hash_to_manifest(tests[id][:manifest_props])
-  if tests[id][:ensure] == :absent
-    state = 'ensure => absent,'
-    tests[id][:resource] = { 'ensure' => 'absent' }
-  else
-    state = 'ensure => present,'
-  end
-  create_interface_title(tests, id)
-
-  tests[id][:manifest] = "cat <<EOF >#{PUPPETMASTER_MANIFESTPATH}
-  \nnode default {
-  cisco_interface { '#{tests[id][:title_pattern]}':
-    #{state}\n#{manifest}
-  }\n}\nEOF"
-
-  cmd = PUPPET_BINPATH +
-        "resource cisco_interface '#{tests[id][:title_pattern]}'"
-  tests[id][:resource_cmd] = cmd
-end
-
-# Wrapper for interface specific settings prior to calling the
-# common test_harness.
-def test_harness_interface(tests, id)
-  return unless platform_supports_test(tests, id)
-
-  tests[id][:ensure] = :present if tests[id][:ensure].nil?
-
-  # Build the manifest for this test
-  build_manifest_interface(tests, id)
-
-  tests[id][:code] = [0, 2]
-
-  interface_cleanup(agent, tests[id][:title_pattern]) if tests[id][:preclean]
-
-  test_harness_common(tests, id)
-  tests[id][:ensure] = nil
-end
-
 #################################################################
 # TEST CASE EXECUTION
 #################################################################
 test_name "TestCase :: #{tests[:resource_name]}" do
-  # -------------------------------------------------------------------
-  device = platform
-  logger.info("#### This device is of type: #{device} #####")
-  resource_absent_cleanup(agent, 'cisco_bridge_domain',
-                          'bridge-domain CLEANUP :: ')
-  logger.info("\n#{'-' * 60}\nSection 1. Default Property Testing")
+  resource_absent_cleanup(agent, 'cisco_bridge_domain')
 
-  test_harness_interface(tests, :default)
+  # -------------------------------------------------------------------
+  logger.info("\n#{'-' * 60}\nSection 1. Default Property Testing")
+  test_harness_run(tests, :default)
 
   # -------------------------------------------------------------------
   logger.info("\n#{'-' * 60}\nSection 2. Non Default Property Testing")
+  test_harness_run(tests, :non_default)
 
-  test_harness_interface(tests, :non_default)
-
-  skipped_tests_summary(tests)
+  interface_cleanup(agent, intf)
 end
 
 logger.info("TestCase :: #{tests[:resource_name]} :: End")
