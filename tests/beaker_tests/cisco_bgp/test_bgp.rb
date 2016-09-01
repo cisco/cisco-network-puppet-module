0c3763d58dd75101a3fcf32f309f462df55e8c87
Rob Gries <robert.w.gries@gmail.com>
2016-09-01 16:27:35 -0400

    Fix suppress fib pending for non-I5 images

--- tests/beaker_tests/cisco_bgp/test_bgp.rb
+++ tests/beaker_tests/cisco_bgp/test_bgp.rb
@@ -65,7 +65,6 @@ tests[:default] = {
     nsr:                                    'default',
     reconnect_interval:                     'default',
     shutdown:                               'default',
-    suppress_fib_pending:                   'default',
     timer_bestpath_limit:                   'default',
     timer_bestpath_limit_always:            'default',
     timer_bgp_holdtime:                     'default',
@@ -98,7 +97,6 @@ tests[:default] = {
     'nsr'                                    => 'false',
     'reconnect_interval'                     => '60',
     'shutdown'                               => 'false',
-    'suppress_fib_pending'                   => 'false',
     'timer_bestpath_limit'                   => '300',
     'timer_bestpath_limit_always'            => 'false',
     'timer_bgp_holdtime'                     => '180',
@@ -217,7 +215,8 @@ def unsupported_properties(tests, id)
         :event_history_periodic <<
         :fast_external_fallover <<
         :flush_routes <<
-        :neighbor_down_fib_accelerate
+        :neighbor_down_fib_accelerate <<
+        :suppress_fib_pending
     end
 
     if platform[/n(5|6|7)k/]
