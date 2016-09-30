63872cd33bac814640d6b55351dae0df310fead1
mikewiebe <mwiebe@cisco.com>
2016-09-30 10:12:15 -0400

    Remove n8k checks and align hash rockets

--- examples/cisco/demo_patching.pp
+++ examples/cisco/demo_patching.pp
@@ -55,11 +55,6 @@ class ciscopuppet::cisco::demo_patching {
       $ciscoPatchName = 'nxos.sample-n9k_EOR-1.0.0-7.0.3.I4.1.lib32_n9000.rpm'
       $ciscoPackageName = 'nxos.sample-n9k_EOR'
     }
-    '7.0(3)F1(1)': {
-      # TBD: Update with real fretta_camden release RPM when ready.
-      $ciscoPatchName = 'nxos.sample-n8k_EOR-1.0.0-7.0.3.F1.1.lib32_nxos.rpm'
-      $ciscoPackageName = 'nxos.sample-n8k_EOR'
-    }
     default:  { fail("\n## NO COMPATIBLE SAMPLE PATCH RPMs AVAILABLE FOR THIS IMAGE: ${image_ver}\n")
     }
   }
