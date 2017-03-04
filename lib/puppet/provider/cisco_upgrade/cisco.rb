f36eac96f1ef145e5ffe66cb40fe07fce519bbab
rahushen <rahul.shenoy.86@gmail.com>
2017-03-04 16:52:34 -0500

    Feature/upgrade services (#427)
    * Add new type for cisco_service
    
    * Add new provider cisco_Service
    
    * Fix rubopcop errors
    
    * fix rubocop errors
    
    * Fix date on CopyRight
    
    * Rename cisco_service to cisco_upgrade
    
    * Redesigned upgrade resource type based on @mikewiebe PR comments
    
    * Redesigned upgrade resource provider based on @mikewiebe PR comments
    
    * removing cisco_service resource type
    
    * rename media to uri and misc. fixes
    
    * converting del_boot_image and force_upgrade to bools
    
    * Remove whitespace
    
    * Source_uri is required
    
    * Handle node_utils API change from service to upgrade

---
+++ lib/puppet/provider/cisco_upgrade/cisco.rb
@@ -0,0 +1,80 @@
+#
+# Puppet provider to manage upgrade of Cisco devices
+#
+# Copyright (c) 2017 Cisco and/or its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'cisco_node_utils' if Puppet.features.cisco_node_utils?
+begin
+  require 'puppet_x/cisco/autogen'
+rescue LoadError # seen on master, not on agent
+  # See longstanding Puppet issues #4248, #7316, #14073, #14149, etc. Ugh.
+  require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..',
+                                     'puppet_x', 'cisco', 'autogen.rb'))
+end
+
+begin
+  require 'puppet_x/cisco/cmnutils'
+rescue LoadError # seen on master, not on agent
+  # See longstanding Puppet issues #4248, #7316, #14073, #14149, etc. Ugh.
+  require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..',
+                                     'puppet_x', 'cisco', 'cmnutils.rb'))
+end
+Puppet::Type.type(:cisco_upgrade).provide(:cisco) do
+  desc 'The Cisco Upgrade provider to upgrade Cisco devices.'
+
+  confine feature: :cisco_node_utils
+  defaultfor operatingsystem: :nexus
+
+  mk_resource_methods
+
+  UPGRADE_NON_BOOL_PROPS = [
+    :version
+  ]
+
+  UPGRADE_ALL_PROPS = UPGRADE_NON_BOOL_PROPS
+
+  PuppetX::Cisco::AutoGen.mk_puppet_methods(:non_bool, self, '@nu',
+                                            UPGRADE_NON_BOOL_PROPS)
+
+  def initialize(value={})
+    super(value)
+    @nu = Cisco::Upgrade
+    @property_flush = {}
+  end
+
+  def self.instances
+    inst = []
+    upgrade = Cisco::Upgrade
+
+    inst << new(
+      name:    'image',
+      version: upgrade.image_version)
+  end
+
+  def self.prefetch(resources)
+    resources.values.first.provider = instances.first
+  end
+
+  def version=(new_version)
+    return if new_version.nil?
+    # Convert del_boot_image and force_upgrade from symbols
+    # to Boolean Class
+    del_boot_image = (@resource[:delete_boot_image] == :true)
+    force_upgrade = (@resource[:force_upgrade] == :true)
+    @nu.upgrade(new_version, @resource[:source_uri][:image_name], @resource[:source_uri][:uri],
+                del_boot_image, force_upgrade)
+    @property_hash[:version] = new_version
+  end
+end
---
+++ lib/puppet/type/cisco_upgrade.rb
@@ -0,0 +1,121 @@
+#
+# Manages the version of Cisco Image running on a device.
+#
+# Copyright (c) 2017 Cisco and/or its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+Puppet::Type.newtype(:cisco_upgrade) do
+  @doc = "Manages the version of Cisco Image running on a device.
+
+  ```
+  cisco_upgrade {\"<instance_name>\":
+    ..attributes..
+  }
+  ```
+
+  There can only be one instance of cisco_upgrade i.e. 'image'
+
+  Example:
+  ```
+    cisco_upgrade {'image' :
+      version           => '7.0(3)I5(1)',
+      source_uri        => 'bootflash:///nxos.7.0.3.I5.1.bin',
+      force_upgrade     => false,
+      delete_boot_image => false,
+    }
+  ```
+  "
+
+  # Parse out the title to fill in the attributes in these
+  # patterns. These attributes can be overwritten later.
+  def self.title_patterns
+    identity = ->(x) { x }
+    patterns = []
+
+    patterns << [
+      /^(\S+)$/,
+      [
+        [:name, identity]
+      ],
+    ]
+    patterns
+  end
+
+  newparam(:name, namevar: :true) do
+    # Parameter used only to satisfy namevar
+    desc 'Name of cisco_upgrade instance. Valid values are string'
+    validate do |name|
+      warning "only 'image' is accepted as a valid name" if name != 'image'
+    end
+  end
+
+  newparam(:source_uri) do
+    examples = "\nExample:\nbootflash:nxos.7.0.3.I5.2.bin"
+    supported = "\nNOTE: Only bootflash: is supported."
+    desc "URI to the image to install on the device. Format <uri>:<image>.
+          Valid values are string.#{examples}#{supported}"
+
+    validate do |uri|
+      fail 'source_uri must match format <uri>:<image>' unless uri[/\S+:\S+/]
+    end
+    munge do |uri|
+      image = {}
+      # Convert <uri>:<image> to a hash.
+      # The Node-utils API expects uri and image_name as two
+      # separate arguments. Pre-processing the arguments here.
+      if uri.include?('/')
+        image[:uri] = uri.split('/')[0]
+        image[:image_name] = uri.split('/')[-1]
+      else
+        image[:uri] = uri.split(':')[0] + ':'
+        image[:image_name] = uri.split(':')[-1]
+      end
+      image
+    end
+  end # param source_uri
+
+  newparam(:force_upgrade) do
+    desc 'Force upgrade the device.'
+    defaultto :false
+    newvalues(:true, :false)
+  end # param force_upgrade
+
+  newparam(:delete_boot_image) do
+    desc 'Delete the booted image(s).'
+    defaultto :false
+    newvalues(:true, :false)
+  end # param delete_boot_image
+
+  ##############
+  # Attributes #
+  ##############
+
+  newproperty(:version) do
+    desc 'Version of the Cisco image to install on the device.
+          Valid values are strings'
+    validate do |ver|
+      fail "Version can't be nil or an empty string" if
+        ver == '' || ver.nil? == :true
+      valid_chars = 'Version can only have the following
+          characters: 0-9, a-z, A-Z, (, ) and .'
+      fail "Invalid version string. #{valid_chars}" unless
+        (/([0-9a-zA-Z().]*)/.match(ver))[0] == ver
+    end
+  end # property version
+
+  validate do
+    fail("The 'source_uri' parameter must be set in the manifest") if
+      self[:source_uri].nil? || self[:source_uri] == ''
+  end
+end
