d4863d2c6f82445fa9561f50da1c5e77a5163123
sai chintalapudi <saichint@cisco.com>
2017-05-04 18:55:00 -0700

    comment cleanup

--- lib/puppet/type/cisco_bgp_af_aa.rb
+++ lib/puppet/type/cisco_bgp_af_aa.rb
@@ -96,12 +96,12 @@ Example:
   end
 
   newparam(:afi, namevar: true) do
-    desc 'BGP Address-family AFI (ipv4|ipv6|vpnv4|vpnv6|l2vpn). Valid values are string.'
+    desc 'BGP Address-family AFI (ipv4|ipv6). Valid values are string.'
     newvalues(:ipv4, :ipv6)
   end
 
   newparam(:safi, namevar: true) do
-    desc 'BGP Address-family SAFI (unicast|multicast|evpn). Valid values are string.'
+    desc 'BGP Address-family SAFI (unicast|multicast). Valid values are string.'
     newvalues(:unicast, :multicast)
   end
 
