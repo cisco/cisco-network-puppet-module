341c1ebbbc669a523f6d0e4971213ab993bc33fe
Rahul Shenoy <rahushen@cisco.com>
2018-01-11 15:43:40 -0500

    add integer validation check for multisite id

--- lib/puppet/type/cisco_evpn_multisite.rb
+++ lib/puppet/type/cisco_evpn_multisite.rb
@@ -36,6 +36,15 @@ Puppet::Type.newtype(:cisco_evpn_multisite) do
   ##############
   newparam(:multisite, namevar: true) do
     desc 'The Evpn Multisite id. Valid values are integer.'
+
+    munge do |value|
+      begin
+        value = Integer(value)
+      rescue
+        raise 'multisite id must be an integer.'
+      end
+      value
+    end
   end
 
   ##############
