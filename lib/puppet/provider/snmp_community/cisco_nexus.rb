e5a5fadab87382de9bb9c4d61d3b2ee595a38122
Thomas-Franklin <thomas.franklin@puppet.com>
2018-10-29 11:35:10 +0000

    (FM-7455) conversion of snmp_community to agentless

---
+++ lib/puppet/provider/snmp_community/cisco_nexus.rb
@@ -0,0 +1,58 @@
+require 'puppet/resource_api/simple_provider'
+
+# Implementation for the snmp_community type using the Resource API.
+class Puppet::Provider::SnmpCommunity::CiscoNexus < Puppet::ResourceApi::SimpleProvider
+  def get(_context, communities=nil)
+    require 'cisco_node_utils'
+    current_states = []
+    if communities.nil? || communities.empty?
+      @snmp_communities ||= Cisco::SnmpCommunity.communities
+      @snmp_communities.each do |community, instance|
+        current_states << {
+          name:   community,
+          ensure: 'present',
+          acl:    instance.acl,
+          group:  instance.group,
+        }
+      end
+    else
+      communities.each do |community|
+        @snmp_communities ||= Cisco::SnmpCommunity.communities
+        individual_community = @snmp_communities[community]
+        next if individual_community.nil?
+        current_states << {
+          name:   community,
+          ensure: 'present',
+          acl:    individual_community.acl,
+          group:  individual_community.group,
+        }
+      end
+    end
+    current_states
+  end
+
+  def delete(context, name)
+    context.notice("Destroying '#{name}'")
+    @snmp_communities ||= Cisco::SnmpCommunity.communities
+    @snmp_communities[name].destroy
+  end
+
+  def update(context, name, should)
+    context.notice("Updating '#{name}' with #{should.inspect}")
+    @snmp_communities ||= Cisco::SnmpCommunity.communities
+    snmp_community = @snmp_communities[name]
+    should_apply(snmp_community, should)
+  end
+
+  def create(context, name, should)
+    context.notice("Creating '#{name}' with #{should.inspect}")
+    snmp_community = Cisco::SnmpCommunity.new(name, should[:group])
+    should_apply(snmp_community, should)
+  end
+
+  def should_apply(snmp_community, should)
+    [:acl, :group].each do |property|
+      snmp_community.send("#{property}=", should[property]) if should[property] && snmp_community.respond_to?("#{property}=")
+    end
+  end
+end
