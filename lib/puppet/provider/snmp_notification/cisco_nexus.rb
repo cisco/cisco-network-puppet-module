3da895b53d9203ba0585b9fb3cbc48e05cb06bca
Thomas-Franklin <thomas.franklin@puppet.com>
2018-10-29 13:36:40 +0000

    (FM-7456) conversion of snmp_notification to agentless

---
+++ lib/puppet/provider/snmp_notification/cisco_nexus.rb
@@ -0,0 +1,55 @@
+module Puppet; end # rubocop:disable Style/Documentation
+module Puppet::ResourceApi
+  # Implementation for the snmp_notification type using the Resource API.
+  class Puppet::Provider::SnmpNotification::CiscoNexus
+    def set(context, changes)
+      changes.each do |name, change|
+        is = if context.type.feature?('simple_get_filter')
+               change.key?(:is) ? change[:is] : (get(context, [name]) || []).find { |r| r[:name] == name }
+             else
+               change.key?(:is) ? change[:is] : (get(context) || []).find { |r| r[:name] == name }
+             end
+        context.type.check_schema(is) unless change.key?(:is)
+
+        should = change[:should]
+
+        if should != is
+          update(context, name, should)
+        end
+      end
+    end
+
+    def get(_context, notifications=nil)
+      require 'cisco_node_utils'
+      current_states = []
+      if notifications.nil? || notifications.empty?
+        @snmp_notifications ||= Cisco::SnmpNotification.notifications
+        @snmp_notifications.each do |notification, instance|
+          current_states << {
+            name:   notification,
+            enable: instance.enable,
+          }
+        end
+      else
+        notifications.each do |notification|
+          @snmp_notifications ||= Cisco::SnmpNotification.notifications
+          individual_notification = @snmp_notifications[notification]
+          next if individual_notification.nil?
+          current_states << {
+            name:   notification,
+            enable: individual_notification.enable,
+          }
+        end
+      end
+      current_states
+    end
+
+    def update(context, name, should)
+      context.notice("Updating '#{name}' with #{should.inspect}")
+      @snmp_notifications ||= Cisco::SnmpNotification.notifications
+      snmp_notification = @snmp_notifications[name]
+      snmp_notification = Cisco::SnmpNotification.new(name) if snmp_notification.nil?
+      snmp_notification.enable = should[:enable] unless should[:enable].nil?
+    end
+  end
+end
