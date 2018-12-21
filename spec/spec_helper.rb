6df878faf6fb520c2b6b1fcbc2f4a8adc0992a1d
Thomas-Franklin <thomas.franklin@puppet.com>
2018-12-21 09:03:38 +0000

    (maint) fixing up the shared examples for the pdksync prep

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,29 +39,6 @@ RSpec.configure do |c|
   c.expect_with(:rspec) { |spec| spec.syntax = :expect }
 end
 
-RSpec.shared_examples 'a noop canonicalizer' do
-  context 'canonicalize is called' do
-    let(:resources) do
-      {
-        name:   'foo',
-        ensure: 'present',
-        foo:    'bar',
-      }
-    end
-    let(:provider) { described_class.new }
-
-    it 'returns the same resource' do
-      expect(provider.canonicalize(anything, resources)[:name].object_id).to eq(resources[:name].object_id)
-      expect(provider.canonicalize(anything, resources)[:ensure].object_id).to eq(resources[:ensure].object_id)
-      expect(provider.canonicalize(anything, resources)[:foo].object_id).to eq(resources[:foo].object_id)
-    end
-
-    it 'returns unmodified resource' do
-      expect(provider.canonicalize(anything, resources)).to eq(name: 'foo', ensure: 'present', foo: 'bar')
-    end
-  end
-end
-
 def ensure_module_defined(module_name)
   module_name.split('::').reduce(Object) do |last_module, next_module|
     last_module.const_set(next_module, Module.new) unless last_module.const_defined?(next_module)
