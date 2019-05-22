1fa1be9e05dcc1e8a4c4e942fa221286090af90d
Will Meek <william.meek@puppet.com>
2019-05-22 15:58:52 +0100

    (maint) Add a function to allow for backwards compatible credentials
    This commit will add a function to allow for backwards compatible credentials.
    - address  (map to host)
    - username (map to user)

--- lib/puppet/util/network_device/cisco_nexus/device.rb
+++ lib/puppet/util/network_device/cisco_nexus/device.rb
@@ -19,8 +19,37 @@ end
 module Puppet::Util::NetworkDevice::Cisco_nexus # rubocop:disable Style/ClassAndModuleCamelCase
   # Wrapper class for `lib/puppet/transport/cisco_nexus.rb` to allow backwards compatiblity
   class Device < Puppet::ResourceApi::Transport::Wrapper
+    def backwards_compatible_schema_load(url_or_config)
+      if url_or_config.is_a? String
+        url = URI.parse(url_or_config)
+        raise "Unexpected url '#{url_or_config}' found. Only file:/// URLs for configuration supported at the moment." unless url.scheme == 'file'
+        raise "Trying to load config from '#{url.path}, but file does not exist." if url && !File.exist?(url.path)
+        config = self.class.deep_symbolize(Hocon.load(url.path, syntax: Hocon::ConfigSyntax::HOCON) || {})
+      elsif url_or_config.is_a? Hash
+        config = url_or_config
+      end
+
+      # Allow for backwards compatibility with the fields
+      # - address  (map to host)
+      # - username (map to user)
+      if config[:address]
+        unless config[:host]
+          config[:host] = config[:address]
+        end
+        config.delete(:address)
+      end
+
+      if config[:username]
+        unless config[:user]
+          config[:user] = config[:username]
+        end
+        config.delete(:username)
+      end
+      config
+    end
+
     def initialize(url_or_config, _options={})
-      super('cisco_nexus', url_or_config)
+      super('cisco_nexus', backwards_compatible_schema_load(url_or_config))
     end
   end
 end
