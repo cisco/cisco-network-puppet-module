4572a354e0ff2da7d7cf916ac740a34da05f4439
mikewiebe <mwiebe@cisco.com>
2019-08-06 12:46:36 -0400

    Add gem check to facter.interface_threshold

--- lib/facter/cisco_nexus.rb
+++ lib/facter/cisco_nexus.rb
@@ -60,14 +60,19 @@ class Facter::CiscoNexus
     # point - it depends on the total number of interfaces on the device - after
     # which it's better to just get all interfaces.
     facts = {}
-    facts['interface_count'] = Cisco::Interface.interface_count
-    if facts['interface_count'] < 1000 && hw_type[/Nexus7/]
-      # N7 uses a less efficient show cmd get_value to workaround an image bug
-      thresh_pct = 0.075
+    if Gem::Version.new(CiscoNodeUtils::VERSION) <= Gem::Version.new('2.0.2')
+      info '## Notice: Please upgrade cisco_node_utils to v2.1.0 or newer'
+      facts['interface_threshold'] = 0
     else
-      thresh_pct = 0.15
+      facts['interface_count'] = Cisco::Interface.interface_count
+      if facts['interface_count'] < 1000 && hw_type[/Nexus7/]
+        # N7 uses a less efficient show cmd get_value to workaround an image bug
+        thresh_pct = 0.075
+      else
+        thresh_pct = 0.15
+      end
+      facts['interface_threshold'] = (facts['interface_count'] * thresh_pct).to_i
     end
-    facts['interface_threshold'] = (facts['interface_count'] * thresh_pct).to_i
     facts
   end
 end
